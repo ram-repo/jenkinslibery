c0b49c146ae72b91a1b27caa9c3d93c9e7ad9e49
ram-repo <ramjagadeesh2205@gmail.com>
2022-03-26 20:42:28 +0530

    Create dsl1.groovy

---
+++ vars/dsl1.groovy
@@ -0,0 +1,55 @@
+import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials
+import com.cloudbees.plugins.credentials.CredentialsProvider
+import hudson.util.Secret
+import hudson.plugins.git.*
+import hudson.model.BuildAuthorizationToken
+import org.apache.commons.lang.RandomStringUtils
+import hudson.tasks.Mailer
+import com.cloudbees.hudson.plugins.folder.Folder
+import hudson.scm.SCM
+import hudson.model.ListView
+import hudson.views.ListViewColumn
+import hudson.triggers.SCMTrigger
+import hudson.*
+import hudson.security.*
+import java.util.*
+import javaposse.jobdsl.dsl.DslFactory
+import com.michelin.cio.hudson.plugins.rolestrategy.*
+import java.lang.reflect.*
+/* groovylint-disable-next-line ClassNameSameAsFilename */
+class MultibranchPipelineJobBuilder {
+
+    String projectView
+    String repoName
+
+    MultibranchWorkflowJob build(DslFactory factory) {
+        factory.multibranchPipelineJob("${projectView}/${repoName}") {
+            branchSources {
+                github {
+                    id('34343434') // IMPORTANT: use a constant and unique identifier
+                    scanCredentialsId('BREP-GitHubApp')
+                    repoOwner('bre-org')
+                    repository("${repoName}")
+                    includes('master feature/* bugfix/* hotfix/* release/*')
+                    excludes('donotbuild/*')
+                }
+            }
+            factory {
+                workflowBranchProjectFactory {
+                    scriptPath('jenkinsFile.groovy')
+                }
+            }
+            triggers {
+                periodicFolderTrigger {
+                    interval('2m')
+                }
+            }
+            orphanedItemStrategy {
+                discardOldItems {
+                    numToKeep(10)
+                }
+            }
+        }
+    }
+
+}
