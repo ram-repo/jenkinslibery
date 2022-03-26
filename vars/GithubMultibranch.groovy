2ee85075d0a39dc2894e3d7312efc986267b7cc8
ram-repo <ramjagadeesh2205@gmail.com>
2022-03-26 23:30:59 +0530

    Update GithubMultibranch.groovy

--- vars/GithubMultibranch.groovy
+++ vars/GithubMultibranch.groovy
@@ -1,22 +1,8 @@
 package mutibranch
-import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials
-import com.cloudbees.plugins.credentials.CredentialsProvider
-import hudson.util.Secret
-import hudson.plugins.git.*
-import hudson.model.BuildAuthorizationToken
-import org.apache.commons.lang.RandomStringUtils
-import hudson.tasks.Mailer
-import com.cloudbees.hudson.plugins.folder.Folder
-import hudson.scm.SCM
-import hudson.model.ListView
-import hudson.views.ListViewColumn
-import hudson.triggers.SCMTrigger
-import hudson.*
-import hudson.security.*
-import java.util.*
+
 import javaposse.jobdsl.dsl.DslFactory
-import com.michelin.cio.hudson.plugins.rolestrategy.*
-import java.lang.reflect.*
+import javaposse.jobdsl.dsl.*
+
 /* groovylint-disable-next-line ClassNameSameAsFilename */
 class MultibranchPipelineJobBuilder {
 
@@ -24,13 +10,13 @@ class MultibranchPipelineJobBuilder {
     String repoName
 
     MultibranchWorkflowJob build(DslFactory factory) {
-        factory.multibranchPipelineJob("${projectView}/${repoName}") {
+        factory.multibranchPipelineJob(projectView) {
             branchSources {
                 github {
                     id('34343434') // IMPORTANT: use a constant and unique identifier
                     scanCredentialsId('BREP-GitHubApp')
                     repoOwner('bre-org')
-                    repository("${repoName}")
+                    repository(repoName)
                     includes('master feature/* bugfix/* hotfix/* release/*')
                     excludes('donotbuild/*')
                 }
