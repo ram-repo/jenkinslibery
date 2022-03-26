5ad5970ce336f35d189ee2427eaf8657ba9c4439
ram-repo <ramjagadeesh2205@gmail.com>
2022-03-27 00:27:40 +0530

    Update mutibranch.groovy

--- vars/mutibranch.groovy
+++ vars/mutibranch.groovy
@@ -1,20 +1,22 @@
-def call() {
+import utilities.GithubMultibranch
 
+
+
+def call() {
+   
 properties([
     parameters([
         choice(name: 'projectsview', description: 'Please pick one', choices: ['dev', 'qa', 'test', 'prod']),
         string(name: 'repoName', description: 'id github ', defaultValue: 'job-dsl-plugin')
     ])])
 
-pipeline {
-    agent any
-    stages{
-        stage('New MutiBranch pipline'){
-            steps{
-                script{
-                    GithubMultibranch.GithubMultibranch("$params.projectsview", "$params.repoName")
-                    }
+stages{
+    stage('New MutiBranch pipline'){
+        steps{
+            script{
+                GithubMultibranch.GithubMultibranch("$params.projectsview", "$params.repoName").build(this)
                 }
+               }
             }
         }
     }
