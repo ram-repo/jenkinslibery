5e1654769a67ef80a05fb7cb192a4a2bfcb4603a
ram-repo <ramjagadeesh2205@gmail.com>
2022-03-27 18:17:14 +0530

    Update and rename dsl1.groovy to working.groovy

---
+++ vars/working.groovy
@@ -0,0 +1,45 @@
+import javaposse.jobdsl.dsl.DslFactory
+import hudson.plugins.git.*
+import hudson.*
+import hudson.security.*
+import java.util.*
+
+def createNewJenkinsJob(String projectName, String destProject) {
+    jobDsl additionalParameters: [
+       // projectsFolder: projectsFolder,
+        projectName: projectName,
+        destProject: destProject,
+        //destGit: destGit,
+        //gitUserUri: gitUser.replace('@', '%40'),
+        //gitServerHost: gitServerHost,
+        //scmCredsID: scmCredsID
+    ], scriptText: '''
+    multibranchPipelineJob("${projectName}") {
+    branchSources {
+        github {
+            id('91179757') // IMPORTANT: use a constant and unique identifier
+            scanCredentialsId('github-ci')
+            repoOwner('ram-repo')
+            repository("${destProject}")
+            includes("master feature/* bugfix/* hotfix/* release/*")
+            excludes("donotbuild/*")
+        }
+    }
+      factory {
+        workflowBranchProjectFactory {
+            scriptPath("jenkinsFile.groovy")
+        }
+      }
+    triggers {
+        periodicFolderTrigger {
+            interval("2m")
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(10)
+        }
+    }
+    }
+    '''
+}
