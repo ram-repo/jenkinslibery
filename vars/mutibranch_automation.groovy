d7e6a22cf974fe2c5c89fbe33d92a65e0f5d7ece
ram-repo <ramjagadeesh2205@gmail.com>
2022-03-30 20:33:28 +0530

    Create mutibranch_automation.groovy

---
+++ vars/mutibranch_automation.groovy
@@ -0,0 +1,81 @@
+
+import javaposse.jobdsl.dsl.DslFactory
+import hudson.plugins.git.*
+import hudson.*
+import hudson.security.*
+import java.util.*
+def call() {
+def createNewJenkinsJob(String projectName, String destProject) {
+    jobDsl additionalParameters: [
+       // projectsFolder: projectsFolder,
+        projectName: projectName,
+        destProject: destProject,
+        //destGit: destGit,
+        //gitUserUri: gitUser.replace('@', '%40'),
+        //gitServerHost: gitServerHost,
+        //scmCredsID: scmCredsID
+    ], scriptText: '''
+    multibranchPipelineJob("${projectName}") {
+    branchSources {
+        github {
+            id('91179757') // IMPORTANT: use a constant and unique identifier
+            scanCredentialsId('github-ci')
+            repoOwner('ram-repo')
+            repository("${destProject}")
+            includes("master feature/* bugfix/* hotfix/* release/*")
+            excludes("donotbuild/*")
+        }
+         configure {
+         def traits = it / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' / 'traits'
+         traits << 'org.jenkinsci.plugins.github__branch__source.ForkPullRequestDiscoveryTrait' {
+         strategyId(1)
+         }
+         traits << 'org.jenkinsci.plugins.github__branch__source.BranchDiscoveryTrait' {
+         strategyId(1)
+         }
+         traits << 'org.jenkinsci.plugins.github__branch__source.OriginPullRequestDiscoveryTrait' {
+         strategyId(1)
+         }
+         traits << 'org.jenkinsci.plugins.github__branch__source.TagDiscoveryTrait' {
+         strategyId(1)
+         }
+       }
+    }
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath("jenkinsFile.groovy")
+        }
+      }
+    triggers {
+        periodicFolderTrigger {
+            interval("2m")
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(10)
+        }
+    }
+}
+    '''
+}
+
+properties([
+    parameters([
+        choice(name: 'projectsview', description: 'Please pick one', choices: ['dev', 'qa', 'test', 'prod']),
+        string(name: 'Name', description: 'RepoName ', defaultValue: 'game-of-life')
+    ])])
+
+pipeline {
+    agent any
+    stages{
+        stage('New MutiBranch pipline'){
+            steps{
+                script{
+                    createNewJenkinsJob("$params.projectsview","$params.Name")
+                   }
+                }
+            }
+        }
+    }
+}
