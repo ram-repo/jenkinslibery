721178c5f02c2b651cd70faba09ec43a21f8fde1
ram-repo <ramjagadeesh2205@gmail.com>
2022-04-08 17:07:54 +0530

    Update working.groovy

--- vars/working.groovy
+++ vars/working.groovy
@@ -30,7 +30,7 @@ def createNewJenkinsJob(String projectName, String destProject) {
          strategyId(1)
          }
          traits << 'org.jenkinsci.plugins.github__branch__source.BranchDiscoveryTrait' {
-         strategyId(1)
+         strategyId(3)
          }
          traits << 'org.jenkinsci.plugins.github__branch__source.OriginPullRequestDiscoveryTrait' {
          strategyId(1)
