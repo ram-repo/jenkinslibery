d7e0133bc36ffeda9fb303ef73a7eb23095af2df
ram-repo <91179757+ram-repo@users.noreply.github.com>
2022-04-11 22:44:58 +0530

    Revert "dd"
    This reverts commit b089838c59638c4c3acf763b382fe0092cada050.

--- vars/working.groovy
+++ vars/working.groovy
@@ -13,7 +13,7 @@ def createNewJenkinsJob(String projectName, String destProject) {
     //gitUserUri: gitUser.replace('@', '%40'),
     //gitServerHost: gitServerHost,
     //scmCredsID: scmCredsID
-    ], scriptText: 
+    ], scriptText: '''
     multibranchPipelineJob("${projectName}") {
     branchSources {
         github {
@@ -35,6 +35,7 @@ def createNewJenkinsJob(String projectName, String destProject) {
             traits << 'org.jenkinsci.plugins.github__branch__source.TagDiscoveryTrait'()
           }
        }
+    }
     strategy {
         defaultBranchPropertyStrategy {
         }
