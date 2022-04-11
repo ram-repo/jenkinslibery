b1e524dbe9172bd57050b5c3ce0c8cb1f1f7beec
ram-repo <ramjagadeesh2205@gmail.com>
2022-04-11 21:02:53 +0530

    Update working.groovy

--- vars/working.groovy
+++ vars/working.groovy
@@ -1,8 +1,12 @@
-import javaposse.jobdsl.dsl.DslFactory
 import hudson.plugins.git.*
 import hudson.*
 import hudson.security.*
 import java.util.*
+import groovy.transform.builder.Builder
+import groovy.transform.builder.SimpleStrategy
+import javaposse.jobdsl.dsl.DslFactory
+import javaposse.jobdsl.dsl.Job
+import javaposse.jobdsl.dsl.jobs.MultibranchWorkflowJob
 
 def createNewJenkinsJob(String projectName, String destProject) {
     jobDsl additionalParameters: [
