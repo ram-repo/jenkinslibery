9e879ea2380ae1235b4045ad310f7689ee7c2c20
ram-repo <ramjagadeesh2205@gmail.com>
2022-04-12 09:08:01 +0530

    Update working.groovy

--- vars/working.groovy
+++ vars/working.groovy
@@ -28,7 +28,7 @@ def createNewJenkinsJob(String projectName, String destProject) {
                 gitHubBranchDiscovery(3), 
                 gitHubPullRequestDiscovery(2), 
                 gitHubForkDiscovery(strategyId: 1, trust: gitHubTrustPermissions()), 
-                gitHubTagDiscovery(3), 
+                gitHubTagDiscovery([]), 
                 [$class: 'RefSpecsSCMSourceTrait', templates: [[value: '+refs/heads/*:refs/remotes/@{remote}/*']]]
             ]
         }
