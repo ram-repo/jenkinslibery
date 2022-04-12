efc67d54c5c4087bdb5b25696a65246c385e4ebb
ram-repo <ramjagadeesh2205@gmail.com>
2022-04-12 09:25:43 +0530

    Update working.groovy

--- vars/working.groovy
+++ vars/working.groovy
@@ -28,7 +28,7 @@ def createNewJenkinsJob(String projectName, String destProject) {
                 gitHubBranchDiscovery(3), 
                 gitHubPullRequestDiscovery(2), 
                 gitHubForkDiscovery(strategyId: 1, trust: gitHubTrustPermissions()), 
-                gitHubTagDiscovery(['release*']), 
+                gitHubTagDiscovery(['release*']) 
                 //[$class: 'gitHubTagDiscovery',  templates: [[value: 'release*']]]
             ]
         }
