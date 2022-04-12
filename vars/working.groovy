48fa205b782c0d0f5928477cb2d8722db70967dc
ram-repo <ramjagadeesh2205@gmail.com>
2022-04-12 08:59:16 +0530

    Update working.groovy

--- vars/working.groovy
+++ vars/working.groovy
@@ -24,9 +24,13 @@ def createNewJenkinsJob(String projectName, String destProject) {
             repository("${destProject}")
             includes("master main feature/* bugfix/* hotfix/* release/*")
             excludes("donotbuild/*")
-            traits: [gitHubBranchDiscovery(3), gitHubPullRequestDiscovery(2), gitHubForkDiscovery(strategyId: 1, 
-            trust: gitHubTrustPermissions()), gitHubTagDiscovery(*), [$class: 'RefSpecsSCMSourceTrait', 
-            templates: [[value: '+refs/heads/*:refs/remotes/@{remote}/*']]]]
+            traits: [
+                gitHubBranchDiscovery(3), 
+                gitHubPullRequestDiscovery(2), 
+                gitHubForkDiscovery(strategyId: 1, trust: gitHubTrustPermissions()), 
+                gitHubTagDiscovery('*'), 
+                [$class: 'RefSpecsSCMSourceTrait', templates: [[value: '+refs/heads/*:refs/remotes/@{remote}/*']]]
+            ]
         }
         configure {
             def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
